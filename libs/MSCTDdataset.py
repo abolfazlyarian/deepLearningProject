3bd80df51695b9f1572e9e16c5e05cbed8ed8bd0
Mehran <mehran_dota@gmail.com>
2023-01-09 18:27:25 +0330

    final version

--- libs/MSCTDdataset.py
+++ libs/MSCTDdataset.py
@@ -10,7 +10,11 @@ def read_data_file(root_dir):
         return: image list: train or val images list
     """
     image_arr = np.array(glob.glob(os.path.join(root_dir, '*.jpg')))
-    image_nums_arr = np.array([int(s.rsplit('/')[-1][0:-4]) for s in image_arr])
+    try:
+        image_nums_arr = np.array([int(s.rsplit('/')[-1][0:-4]) for s in image_arr])
+    except :
+        image_nums_arr = np.array([int(s.rsplit('\\')[-1][0:-4]) for s in image_arr])
+
     sorted_image_arr = image_arr[np.argsort(image_nums_arr)]
     return sorted_image_arr.tolist()
 
