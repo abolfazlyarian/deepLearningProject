d6b88c16402382b3d1f5f4801b68094ecb81ef4b
Mehran <100010780+MehranMorabiPazoki@users.noreply.github.com>
2023-01-25 09:07:02 +0330

    Update MixFace.py

--- networks/MixFace.py
+++ networks/MixFace.py
@@ -5,18 +5,13 @@ import torch.nn.init as init
 from torchvision import models
 from collections import OrderedDict
 
+
 class MixFaceMLP(nn.Module):
-    def __init__(self,max_num=6) -> None:
+    def __init__(self,dim=6) -> None:
         super(MixFaceMLP,self).__init__()
-        self.model=[]
-        for i in range(2,max_num+1):
-            self.model.append(nn.Sequential(OrderedDict([('Linear_1',nn.Linear(i*3,10)),('relu_1',nn.ReLU())
+        self.model=(nn.Sequential(OrderedDict([('flatten',nn.Flatten(0)),('Linear_1',nn.Linear(dim*3,10)),('relu_1',nn.ReLU())
             ,('Linear_2',nn.Linear(10,3))])))
-            
+        
     def forward(self,x):
-        i=len(x)
-        if i==1:
-            return x
-        else:
-            x=self.model[i-2](x)
-            return x
+
+      return self.model(x)
