01f03bd5449dfa3474e8bfeb0c17f9888b2e4e03
Mehran <100010780+MehranMorabiPazoki@users.noreply.github.com>
2023-01-29 12:19:28 +0330

    update face_extractor

--- libs/faceExtractor.py
+++ libs/faceExtractor.py
@@ -6,40 +6,45 @@ from insightface.data import get_image as ins_get_image
 import os
 from tqdm import tqdm
 from PIL import Image
-
 class faceExtractor():
     def __init__(self,rootDir,mode="train") -> None:
         self.root=rootDir
-
+        self.mode=mode
 
         if not os.path.exists(f"{self.root}/FaceDataset"):
           os.mkdir(f"{self.root}/FaceDataset")
         if not os.path.exists(f"{self.root}/FaceDataset/faceImage"):
           os.mkdir(f"{self.root}/FaceDataset/faceImage")
         if not os.path.exists(f"{self.root}/FaceDataset/faceImage/"+mode):
-          os.mkdir(f"{self.root}/FaceDataset/faceImage"+mode)
+          os.mkdir(f"{self.root}/FaceDataset/faceImage/"+mode)
         # if not os.path.exists(f"{self.root}/FaceDataset/labels"):
         #   os.mkdir(f"{self.root}/FaceDataset/labels")
-
         self.fault=0
-        self.fault_list=[]
+        self.idx_fault=[]
+        self.max_len=0
         self.app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'],allowed_modules=['detection'])
     def run(self,Dataset):
-        self.app.prepare(ctx_id=0)
+        self.app.prepare(ctx_id=0,det_size=(640,960))
         for img,_,sentiment,idx in tqdm(Dataset):
             faces = self.app.get(img[0])
             img_PIL=Image.fromarray(img[0].astype(np.uint8))
-            if len(faces)==0:
-              self.fault+=1
-              self.fault_list.append(idx)
-            else :
-              if len(faces)>2:
-                a=2
-              else:
-                a=len(faces)
+            face_count=len(faces)
+
+            if face_count > self.max_len:
+              self.max_len = face_count
+
+            if face_count==0:
+                self.fault+=1
+                self.idx_fault.append(idx)
+
+            else:
+
+              if face_count>6:
+                a = 6
+              else :
+                a = face_count
 
               for i,f in zip(range(a),faces):
                   face=img_PIL.crop(f['bbox'])
-                  face.save(f"{self.root}/FaceDataset/faceImage/"+f'{idx}_{i}.jpg')
+                  face.save(f"{self.root}/FaceDataset/faceImage/{self.mode}/"+f'{idx}_{i}.jpg')
                 
-
