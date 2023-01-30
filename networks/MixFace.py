ce594087dc7db141f80521f5e4932a4b5a49444c
Mehran <100010780+MehranMorabiPazoki@users.noreply.github.com>
2023-01-30 15:17:15 +0330

    fix bugs

--- networks/MixFace.py
+++ networks/MixFace.py
@@ -11,7 +11,7 @@ class MixFaceMLP(nn.Module):
         super(MixFaceMLP,self).__init__()
         self.model=(nn.Sequential(OrderedDict([('flatten',nn.Flatten(0)),('Linear_1',nn.Linear(dim*3,10)),('relu_1',nn.ReLU())
             ,('Linear_2',nn.Linear(10,3))])))
-        
+        self.dim=dim
     def forward(self,x):
 
-      return self.model(x)
+        return self.model(F.pad(x,(0,0,0,self.dim-len(x)),"constant", 0))
